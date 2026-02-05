a1d5b0a05fe2917f2bd38dbb77178fa4200bb96c
Volodymyr <panillich@gmail.com>
2026-02-05 12:16:47 +0100

    Fix print statement in Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
         int i = 5;
-        System.out.println(i);
+        System.out.println("Hello test_!);
     }
 }
